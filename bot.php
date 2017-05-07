a98847a6b8fcbe0e896f511195ff6d004f9fd168
satrawo <st.sorawat38@gmail.com>
2017-05-07 16:49:57 +0700

    Modified 2nd file

--- bot.php
+++ bot.php
@@ -1,2 +1,12 @@
-<<?php
+<?php
 echo "I am a bot";
+
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'my_voice_is_my_password_verfiy_me') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
