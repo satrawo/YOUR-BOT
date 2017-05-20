75ffae34b8e4fbc1a23a0222873cf59b534fdf98
satrawo <st.sorawat38@gmail.com>
2017-05-20 22:18:17 +0700

    Edited

--- bot.php
+++ bot.php
@@ -17,6 +17,7 @@ $message_to_reply = '';
 /* Recieve the message */
 if(preg_match('[time|current time|now]', strtolower($message))) {
 	$message_to_reply = date(D M d h:i:s);
+
 } 
 else {
     $message_to_reply = 'Huh! what do you mean?';
