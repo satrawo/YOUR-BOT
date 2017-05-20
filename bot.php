be28b0a10fe49c53439578a52acae10cf021c9bf
satrawo <st.sorawat38@gmail.com>
2017-05-20 21:05:29 +0700

    Edited

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
 
+$access_token = "EAADe2JW07d4BAKtEMGXmhf75gES4WHly98Cv3rEYcaQYOcTsPl0NZAGqlNu8n2t2AwW7dD0O4MZAN91JrGfKdyYGrRKRY3PfNr4AzDoU0VIT1pWpXtob9f0LT5pS3ekfzmrDkIYWN7uFOuwD7AC62zGxKM1NSUZATqI566bZCQZDZD";
 
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
@@ -9,4 +10,45 @@ if ($verify_token === 'abc123') {
 }
 
 $input = json_decode(file_get_contents('php://input'), true);
-error_log(print_r($input, true));
+$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+$message = $input['entry'][0]['messaging'][0]['message']['text'];
+$message_to_reply = '';
+/**
+ * Some Basic rules to validate incoming messages
+ */
+if(preg_match('[time|current time|now]', strtolower($message))) {
+    // Make request to Time API
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $message_to_reply = $result;
+    }
+} else {
+    $message_to_reply = 'Huh! what do you mean?';
+}
+//API Url
+$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+//Initiate cURL.
+$ch = curl_init($url);
+//The JSON data.
+$jsonData = '{
+    "recipient":{
+        "id":"'.$sender.'"
+    },
+    "message":{
+        "text":"'.$message_to_reply.'"
+    }
+}';
+//Encode the array into JSON.
+$jsonDataEncoded = $jsonData;
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+//Execute the request
+if(!empty($input['entry'][0]['messaging'][0]['message'])){
+    $result = curl_exec($ch);
+}
