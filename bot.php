fd37a1ec87116885710c6730dd59719217f508bf
satrawo <st.sorawat38@gmail.com>
2017-05-20 22:03:14 +0700

    Edited

--- bot.php
+++ bot.php
@@ -13,18 +13,10 @@ $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 $message_to_reply = '';
-/**
- * Some Basic rules to validate incoming messages
- */
+
+/* Recieve the message */
 if(preg_match('[time|current time|now]', strtolower($message))) {
-    // Make request to Time API
-    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-    if($result != '') {
-        $message_to_reply = $result;
-    }
-    else
-    	$message_to_reply = 'Not found';
+    $message_to_reply = time();
 } 
 else {
     $message_to_reply = 'Huh! what do you mean?';
