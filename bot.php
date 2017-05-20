51364b994e80273fc54ce7bc021b0fc6455c5a80
satrawo <st.sorawat38@gmail.com>
2017-05-20 21:38:47 +0700

    Edited

--- bot.php
+++ bot.php
@@ -16,16 +16,11 @@ $message_to_reply = '';
 /**
  * Some Basic rules to validate incoming messages
  */
-if(preg_match('[time|current time|now]', strtolower($message))) {
-    // Make request to Time API
-    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
-    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
-    if($result != '') {
-        $message_to_reply = $result;
+if (! isset($messages))  {
+    $result = $messages;
+    $message_to_reply = $result;
     }
-} else {
-    $message_to_reply = 'Huh! what do you mean?';
-}
+
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 //Initiate cURL.
@@ -41,12 +36,16 @@ $jsonData = '{
 }';
 //Encode the array into JSON.
 $jsonDataEncoded = $jsonData;
+
 //Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
+
 //Attach our encoded JSON string to the POST fields.
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
