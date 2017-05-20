44f958dcc0a56191b2469773a127f4464c126fb9
satrawo <st.sorawat38@gmail.com>
2017-05-20 21:55:26 +0700

    Edited

--- bot.php
+++ bot.php
@@ -16,13 +16,19 @@ $message_to_reply = '';
 /**
  * Some Basic rules to validate incoming messages
  */
-if (isset($messages))  {
-    $result = $messages;
-    $message_to_reply = $result;
+if(preg_match('[time|current time|now]', strtolower($message))) {
+    // Make request to Time API
+    ini_set('user_agent','Mozilla/4.0 (compatible; MSIE 6.0)');
+    $result = file_get_contents("http://www.timeapi.org/utc/now?format=%25a%20%25b%20%25d%20%25I:%25M:%25S%20%25Y");
+    if($result != '') {
+        $message_to_reply = $result;
     }
-else 
+    else
+    	$message_to_reply = 'Not found';
+} 
+else {
     $message_to_reply = 'Huh! what do you mean?';
-
+}
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
 //Initiate cURL.
@@ -47,7 +53,6 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
